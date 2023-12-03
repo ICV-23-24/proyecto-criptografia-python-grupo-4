5ba3d01c9e95c8da1db377fe6770763a26177ae0
PedroP90 <ppergim954@g.educand.es>
2023-12-03 20:29:24 +0100

    aeslocal

--- app.py
+++ app.py
@@ -19,31 +19,39 @@ def csimetrico():
         archivos = request.files.getlist('archivo')
         # Obtiene el archivo seleccionado en el desplegable
         archivo2 = request.form.get('archivo')
-        key = request.form['key']
+        key_decrypt = request.form.get('key_decrypt')
+        key = request.form.get('key')
         mode = request.form['mode']
         # Inicializa una variable donde están los archivos
         listado_archivos = []
+        listado_claves = []
         # Determina el destino de los ficheros donde se guardarán los mensajes encriptados
         ruta = './archivos'
+        rutaclaves = './claves'
 
         if mode == 'encrypt':
             # Seleccionamos el primer fichero
             docu = archivos[0]
             # Lee el contenido y lo guardo en una variable
             contenido = docu.read().decode('utf-8')
-            # Crea una variable donde uso el contenido y la llave para encriptar el texto de la variable contenido
-            encrypted_message = f.encrypt_message(contenido, key)
             # Recoge el nombre del fichero del formulario
             nuevo_nombre = request.form['archivo_nombre']
             # Añade una extensión a tu elección, por ejemplo ".txt"
             filename = secure_filename(nuevo_nombre + "_encriptado.txt")
+            clave = f.generar_cadena()
+            nombre_archivo = secure_filename(key + "_clave.txt")
+            with open(os.path.join(rutaclaves, nombre_archivo), "w") as file2:
+                file2.write(clave)
+            # Crea una variable donde uso el contenido y la llave para encriptar el texto de la variable contenido
+            encrypted_message = f.encrypt_message(contenido, clave)
             # Abre el archivo para escritura
             with open(os.path.join(ruta, filename), "w") as file:
                 # Escribe el mensaje encriptado en el archivo
                 file.write(encrypted_message)
             # Listamos los ficheros contenido en la ruta
             listado_archivos = f.listar(ruta)
-            return render_template('csimetrico.html', listado_archivos=listado_archivos,encrypted_message=encrypted_message,contenido=contenido, mode=mode)
+            listado_claves = f.listarclaves(rutaclaves)
+            return render_template('csimetrico.html',listado_claves=listado_claves, listado_archivos=listado_archivos,encrypted_message=encrypted_message,contenido=contenido, mode=mode)
         elif mode == 'decrypt':
             # Llamos a la función listar para obtener un listado de los archivos en la variable ruta
             listado_archivos = f.listar(ruta)
@@ -52,33 +60,23 @@ def csimetrico():
             # Obtiene el contenido del fichero que seleccionamos en el desplegable
             with open(ruta_completa, 'r') as file:
                 contenido2 = file.read()
-            # Finalmente desencriptamos el contenido del fichero seleccionado
-            decrypted_message = f.decrypt_message(contenido2, key)
-            return render_template('csimetrico.html', listado_archivos=listado_archivos,contenido2=contenido2,decrypted_message=decrypted_message, mode=mode)
+            # Ruta de destino para las claves
+            rutaclaves = './claves'
+            # Listamos el directorio con las claves
+            listado_claves = f.listarclaves(rutaclaves)
+            # Ruta con el fichero de la clave del desplegable
+            ruta_completa2 = os.path.join(rutaclaves, key_decrypt)
+            # Abrimos el fichero con la clave y leemos su contenido
+            with open(ruta_completa2, 'r') as file2:
+                contenidoclave = file2.read()
+            # Finalmente desencriptamos el contenido del fichero seleccionado con la clave y el fichero elegidos
+            decrypted_message = f.decrypt_message(contenido2, contenidoclave)
+            return render_template('csimetrico.html', listado_claves=listado_claves, listado_archivos=listado_archivos,contenido2=contenido2,decrypted_message=decrypted_message, mode=mode)
 
     return render_template("csimetrico.html")
 
-@app.route("/casimetrico/", methods=['GET','POST'])
+@app.route("/casimetrico/")
 def casimetrico():
-
-    global encrypted_message
-
-    if request.method == 'POST':
-        message = request.form['message']
-        private_key, public_key = x.generate_key()
-        print("Public Key:", public_key)
-        print("Private Key:", private_key)
-        # key = request.form['key']
-        mode = request.form['mode']
-
-        if mode == 'encrypt':
-            encrypted_message = x.encrypt(message, public_key)
-            return render_template('casimetrico.html', encrypted_message=encrypted_message, mode=mode)
-        elif mode == 'decrypt':
-            decrypted_message = x.decrypt(encrypted_message, private_key)
-            return render_template('casimetrico.html', decrypted_message=decrypted_message, mode=mode)
-
-
     return render_template("casimetrico.html")
 
 
@@ -110,4 +108,7 @@ def hello_there(name = None):
 def get_data():
     return app.send_static_file("data.json")
 
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
 
--- functions.py
+++ functions.py
@@ -3,15 +3,17 @@ from Cryptodome.Util.Padding import pad,unpad
 from base64 import b64encode, b64decode
 import os
 from os.path import isfile, join
+import string
+import random
 
-def encrypt_message(contenido, key):
-    key = key.encode('utf-8')
+def encrypt_message(contenido, clave):
+    key = clave.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
     encrypted_message = cipher.encrypt(pad(contenido.encode('utf-8'), AES.block_size))
     return b64encode(encrypted_message).decode('utf-8')
 
-def decrypt_message(contenido2, key):
-    key = key.encode('utf-8')
+def decrypt_message(contenido2, contenidoclave):
+    key = contenidoclave.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
     decrypted_message = unpad(cipher.decrypt(b64decode(contenido2)), AES.block_size).decode('utf-8')
     return decrypted_message
@@ -20,3 +22,15 @@ def decrypt_message(contenido2, key):
 def listar(ruta):
     archivos = [a for a in os.listdir(ruta) if isfile(join(ruta, a))]
     return archivos
+
+def listarclaves(rutaclaves):
+    keys = [a for a in os.listdir(rutaclaves) if isfile(join(rutaclaves, a))]
+    return keys
+
+# Generar una cadena de caracteres aleatoria
+def generar_cadena(longitud=16):
+    # Genera una cadena aleatoria usando letras y números
+    caracteres = string.ascii_letters + string.digits
+    cadena_aleatoria = ''.join(random.choice(caracteres) for _ in range(longitud))
+
+    return cadena_aleatoria
