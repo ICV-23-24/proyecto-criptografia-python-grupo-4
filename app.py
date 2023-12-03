bed6047c702ba6b2628eaebb0fae2baccb992b94
PedroP90 <ppergim954@g.educand.es>
2023-12-03 17:47:17 +0100

    AESsinclave

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import os
 from flask import Flask, render_template, request
 import functions as f
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -13,16 +15,46 @@ def home():
 @app.route("/csimetrico/", methods=['GET','POST'])
 def csimetrico():
     if request.method == 'POST':
-        message = request.form['message']
+        # Obtiene una lista de archivos enviados con el formulario
+        archivos = request.files.getlist('archivo')
+        # Obtiene el archivo seleccionado en el desplegable
+        archivo2 = request.form.get('archivo')
         key = request.form['key']
         mode = request.form['mode']
+        # Inicializa una variable donde están los archivos
+        listado_archivos = []
+        # Determina el destino de los ficheros donde se guardarán los mensajes encriptados
+        ruta = './archivos'
 
         if mode == 'encrypt':
-            encrypted_message = f.encrypt_message(message, key)
-            return render_template('csimetrico.html', encrypted_message=encrypted_message, mode=mode)
+            # Seleccionamos el primer fichero
+            docu = archivos[0]
+            # Lee el contenido y lo guardo en una variable
+            contenido = docu.read().decode('utf-8')
+            # Crea una variable donde uso el contenido y la llave para encriptar el texto de la variable contenido
+            encrypted_message = f.encrypt_message(contenido, key)
+            # Recoge el nombre del fichero del formulario
+            nuevo_nombre = request.form['archivo_nombre']
+            # Añade una extensión a tu elección, por ejemplo ".txt"
+            filename = secure_filename(nuevo_nombre + "_encriptado.txt")
+            # Abre el archivo para escritura
+            with open(os.path.join(ruta, filename), "w") as file:
+                # Escribe el mensaje encriptado en el archivo
+                file.write(encrypted_message)
+            # Listamos los ficheros contenido en la ruta
+            listado_archivos = f.listar(ruta)
+            return render_template('csimetrico.html', listado_archivos=listado_archivos,encrypted_message=encrypted_message,contenido=contenido, mode=mode)
         elif mode == 'decrypt':
-            decrypted_message = f.decrypt_message(message, key)
-            return render_template('csimetrico.html', decrypted_message=decrypted_message, mode=mode)
+            # Llamos a la función listar para obtener un listado de los archivos en la variable ruta
+            listado_archivos = f.listar(ruta)
+            # Obtiene el fichero seleccionado en el desplegable con su ruta completa
+            ruta_completa = os.path.join(ruta, archivo2)
+            # Obtiene el contenido del fichero que seleccionamos en el desplegable
+            with open(ruta_completa, 'r') as file:
+                contenido2 = file.read()
+            # Finalmente desencriptamos el contenido del fichero seleccionado
+            decrypted_message = f.decrypt_message(contenido2, key)
+            return render_template('csimetrico.html', listado_archivos=listado_archivos,contenido2=contenido2,decrypted_message=decrypted_message, mode=mode)
 
     return render_template("csimetrico.html")
 
@@ -58,3 +90,5 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+
--- functions.py
+++ functions.py
@@ -1,15 +1,22 @@
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad,unpad
 from base64 import b64encode, b64decode
+import os
+from os.path import isfile, join
 
-def encrypt_message(message, key):
+def encrypt_message(contenido, key):
     key = key.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
-    encrypted_message = cipher.encrypt(pad(message.encode('utf-8'), AES.block_size))
+    encrypted_message = cipher.encrypt(pad(contenido.encode('utf-8'), AES.block_size))
     return b64encode(encrypted_message).decode('utf-8')
 
-def decrypt_message(message, key):
+def decrypt_message(contenido2, key):
     key = key.encode('utf-8')
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
-    decrypted_message = unpad(cipher.decrypt(b64decode(message)), AES.block_size).decode('utf-8')
+    decrypted_message = unpad(cipher.decrypt(b64decode(contenido2)), AES.block_size).decode('utf-8')
     return decrypted_message
+
+# Función para listar solo ficheros con el argumento ruta que pasa la carpeta a listar
+def listar(ruta):
+    archivos = [a for a in os.listdir(ruta) if isfile(join(ruta, a))]
+    return archivos
