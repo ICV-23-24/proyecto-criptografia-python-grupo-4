c599fc23f0d56be0eb109dfffd33a85c5e96d9b5
valvfue <victoralvarezinformatica@gmail.com>
2023-12-07 17:17:35 +0100

    Implementación 3DES

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import os
 from flask import Flask, render_template, request
 import functions as f
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -10,19 +12,57 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/csimetrico/", methods=['GET','POST'])
+@app.route('/csimetrico', methods=['GET', 'POST'])
 def csimetrico():
     if request.method == 'POST':
-        message = request.form['message']
-        key = request.form['key']
+        archivos = request.files.getlist('archivo')
+        archivo2 = request.form.get('archivo')
+        key_decrypt = request.form.get('key_decrypt')
+        key = request.form.get('key')
         mode = request.form['mode']
+        listado_archivos = []
+        listado_claves = []
+        ruta = './archivos'
+        rutaclaves = './claves'
 
         if mode == 'encrypt':
-            encrypted_message = f.encrypt_message(message, key)
-            return render_template('csimetrico.html', encrypted_message=encrypted_message, mode=mode)
+            docu = archivos[0]
+            contenido = docu.read().decode('utf-8')
+            nuevo_nombre = request.form['archivo_nombre']
+            filename = secure_filename(nuevo_nombre + "_encriptado.txt")
+            algoritmo = request.form['algoritmo']  # Nuevo campo para seleccionar el algoritmo
+
+            if algoritmo not in ['AES', '3DES']:
+                return render_template('csimetrico.html', error="Algoritmo no válido")
+
+            clave = f.generar_cadena()  # Genera una clave aleatoria (puedes cambiarlo)
+            nombre_archivo = secure_filename(key + "_clave.txt")
+            with open(os.path.join(rutaclaves, nombre_archivo), "w") as file2:
+                file2.write(clave)
+
+            encrypted_message = f.encrypt_message(contenido, clave, algoritmo)
+            with open(os.path.join(ruta, filename), "w") as file:
+                file.write(encrypted_message)
+
+            listado_archivos = f.listar(ruta)
+            listado_claves = f.listarclaves(rutaclaves)
+            return render_template('csimetrico.html', listado_claves=listado_claves, listado_archivos=listado_archivos,
+                                   encrypted_message=encrypted_message, contenido=contenido, mode=mode)
         elif mode == 'decrypt':
-            decrypted_message = f.decrypt_message(message, key)
-            return render_template('csimetrico.html', decrypted_message=decrypted_message, mode=mode)
+            listado_archivos = f.listar(ruta)
+            ruta_completa = os.path.join(ruta, archivo2)
+            with open(ruta_completa, 'r') as file:
+                contenido2 = file.read()
+
+            rutaclaves = './claves'
+            listado_claves = f.listarclaves(rutaclaves)
+            ruta_completa2 = os.path.join(rutaclaves, key_decrypt)
+            with open(ruta_completa2, 'r') as file2:
+                contenidoclave = file2.read()
+
+            decrypted_message = f.decrypt_message(contenido2, contenidoclave, algoritmo)
+            return render_template('csimetrico.html', listado_claves=listado_claves, listado_archivos=listado_archivos,
+                                   contenido2=contenido2, decrypted_message=decrypted_message, mode=mode)
 
     return render_template("csimetrico.html")
 
@@ -58,3 +98,7 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- functions.py
+++ functions.py
@@ -1,15 +1,87 @@
-from Cryptodome.Cipher import AES
+from Cryptodome.Cipher import AES,DES3
 from Cryptodome.Util.Padding import pad,unpad
 from base64 import b64encode, b64decode
+import os
+from os.path import isfile, join
+import string
+import random
 
-def encrypt_message(message, key):
-    key = key.encode('utf-8')
-    cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
-    encrypted_message = cipher.encrypt(pad(message.encode('utf-8'), AES.block_size))
+# def encrypt_message(contenido, clave):
+#     key = clave.encode('utf-8')
+#     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
+#     encrypted_message = cipher.encrypt(pad(contenido.encode('utf-8'), AES.block_size))
+#     return b64encode(encrypted_message).decode('utf-8')
+
+# def decrypt_message(contenido2, contenidoclave):
+#     key = contenidoclave.encode('utf-8')
+#     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
+#     decrypted_message = unpad(cipher.decrypt(b64decode(contenido2)), AES.block_size).decode('utf-8')
+#     return decrypted_message
+
+# Función para listar solo ficheros con el argumento ruta que pasa la carpeta a listar
+def listar(ruta):
+    archivos = [a for a in os.listdir(ruta) if isfile(join(ruta, a))]
+    return archivos
+
+def listarclaves(rutaclaves):
+    keys = [a for a in os.listdir(rutaclaves) if isfile(join(rutaclaves, a))]
+    return keys
+
+# Generar una cadena de caracteres aleatoria
+def generar_cadena(longitud=16):
+    # Genera una cadena aleatoria usando letras y números
+    caracteres = string.ascii_letters + string.digits
+    cadena_aleatoria = ''.join(random.choice(caracteres) for _ in range(longitud))
+
+    return cadena_aleatoria
+
+# def encrypt_message_3des(contenido, clave):
+#     # Asegúrate de que la clave tenga una longitud válida para 3DES (16, 24 o 32 bytes)
+#     key = clave.encode('utf-8')[:24]
+
+#     # Crea un objeto de cifrado 3DES en modo ECB
+#     cipher = DES3.new(key, DES3.MODE_ECB)
+
+#     # Aplica el relleno al contenido y cifra el mensaje
+#     encrypted_message = cipher.encrypt(pad(contenido.encode('utf-8'), DES3.block_size))
+
+#     # Codifica el mensaje cifrado en base64 y devuelve el resultado como una cadena
+#     return b64encode(encrypted_message).decode('utf-8')
+
+# def decrypt_message_3des(contenido, contenidoclave):
+#     # Asegúrate de que la clave tenga una longitud válida para 3DES (16, 24 o 32 bytes)
+#     key = contenidoclave.encode('utf-8')[:24]
+
+#     # Crea un objeto de cifrado 3DES en modo ECB
+#     cipher = DES3.new(key, DES3.MODE_ECB)
+
+#     # Descodifica el mensaje cifrado en base64 y luego descifra el mensaje
+#     decrypted_message = unpad(cipher.decrypt(b64decode(contenido)), DES3.block_size).decode('utf-8')
+
+#     # Devuelve el mensaje descifrado
+#     return decrypted_message
+
+def encrypt_message(contenido, clave, algoritmo):
+    if algoritmo == 'AES':
+        cipher = AES.new(pad(clave.encode('utf-8'), AES.block_size), AES.MODE_ECB)
+    elif algoritmo == '3DES':
+        clave = clave.encode('utf-8')[:24]  # Asegurar que la clave tenga longitud válida para 3DES
+        cipher = DES3.new(clave, DES3.MODE_ECB)
+    else:
+        raise ValueError("Algoritmo de cifrado no válido")
+
+    encrypted_message = cipher.encrypt(pad(contenido.encode('utf-8'), max(AES.block_size, DES3.block_size)))
     return b64encode(encrypted_message).decode('utf-8')
 
-def decrypt_message(message, key):
-    key = key.encode('utf-8')
-    cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
-    decrypted_message = unpad(cipher.decrypt(b64decode(message)), AES.block_size).decode('utf-8')
+# Función para desencriptar un mensaje usando AES o 3DES
+def decrypt_message(contenido, contenidoclave, algoritmo):
+    if algoritmo == 'AES':
+        cipher = AES.new(pad(contenidoclave.encode('utf-8'), AES.block_size), AES.MODE_ECB)
+    elif algoritmo == '3DES':
+        contenidoclave = contenidoclave.encode('utf-8')[:24]  # Asegurar que la clave tenga longitud válida para 3DES
+        cipher = DES3.new(contenidoclave, DES3.MODE_ECB)
+    else:
+        raise ValueError("Algoritmo de cifrado no válido")
+
+    decrypted_message = unpad(cipher.decrypt(b64decode(contenido)), max(AES.block_size, DES3.block_size)).decode('utf-8')
     return decrypted_message
