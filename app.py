00e5184c7153d2f2d8dfcc7303b685b3d0a0c4f6
valvfue <victoralvarezinformatica@gmail.com>
2023-12-10 23:14:05 +0100

    Corrección de rutas

--- app.py
+++ app.py
@@ -67,93 +67,70 @@ def csimetrico():
 
     return render_template("csimetrico.html")
 
-@app.route("/casimetrico/")
+@app.route('/casimetrico', methods=['GET', 'POST'])
 def casimetrico():
-    # Lee los nombres de los archivos en la carpeta "claves"
-    keys = os.listdir('claves')
-    # Lee los nombres de los archivos en la carpeta "archivos"
-    files = os.listdir('archivos')
-    # Pasa las listas de claves y archivos a la plantilla
-    return render_template("casimetrico.html", keys=keys, files=files)
-
-@app.route("/generate_keys", methods=["POST"])
-def generate_keys():
-    private_key, public_key = f.generate_keys()
-    # Guarda las claves en la carpeta "claves"
-    with open(os.path.join('claves', 'private_key.pem'), 'wb') as private_key_file:
-        private_key_file.write(f.export_private_key(private_key))
-    with open(os.path.join('claves', 'public_key.pem'), 'wb') as public_key_file:
-        public_key_file.write(f.export_public_key(public_key))
-    # Lee los nombres de los archivos en la carpeta "claves"
-    keys = os.listdir('claves')
-    return render_template("casimetrico.html", keys=keys)
-
-@app.route("/import_key", methods=["POST"])
-def import_key():
-    public_key_file = request.files["public_key"]
-    public_key = f.import_public_key(public_key_file.read())
-    # Guarda la clave pública en la carpeta "claves"
-    with open(os.path.join('claves', 'imported_public_key.pem'), 'wb') as imported_public_key_file:
-        imported_public_key_file.write(f.export_public_key(public_key))
-    return "Clave pública importada con éxito"
-
-@app.route("/export_key", methods=["GET"])
-def export_key():
-    with open(os.path.join('claves', 'public_key.pem'), 'rb') as public_key_file:
-        public_key = f.import_public_key(public_key_file.read())
-    response = make_response(f.export_public_key(public_key))
-    response.headers.set('Content-Type', 'application/octet-stream')
-    response.headers.set('Content-Disposition', 'attachment', filename='public_key.pem')
-    return response
-
-@app.route("/encrypt_message", methods=["POST"])
-def encrypt_message():
-    message = request.form["message"].encode('utf-8')
-    with open(os.path.join('claves', 'public_key.pem'), 'rb') as public_key_file:
-        public_key = f.import_public_key(public_key_file.read())
-    encrypted_message = f.encrypt_message(public_key, message)
-    return jsonify({"encrypted_message": base64.b64encode(encrypted_message).decode('utf-8')})
-
-@app.route("/decrypt_message", methods=["POST"])
-def decrypt_message():
-    encrypted_message = base64.b64decode(request.form["encrypted_message"])
-    with open(os.path.join('claves', 'private_key.pem'), 'rb') as private_key_file:
-        private_key = f.import_private_key(private_key_file.read())
-    original_message = f.decrypt_message(private_key, encrypted_message)
-    return jsonify({"original_message": original_message.decode('utf-8')})
-
-@app.route("/encrypt_file", methods=["POST"])
-def encrypt_file():
-    if 'file' not in request.files:
-        return "No se subió ningún archivo", 400
-    file = request.files['file']
-    # Lee la clave pública
-    with open(os.path.join('claves', 'public_key.pem'), 'rb') as public_key_file:
-        public_key = f.import_public_key(public_key_file.read())
-    # Encripta el archivo
-    encrypted_file = f.encrypt_file(public_key, file)
-    # Guarda el archivo encriptado
-    with open(os.path.join('archivos', file.filename), 'wb') as encrypted_file_file:
-        encrypted_file_file.write(encrypted_file)
-    return redirect(url_for('casimetrico'))
-
-@app.route("/decrypt_file", methods=["POST"])
-def decrypt_file():
-    if 'file' not in request.files:
-        return "No se subió ningún archivo", 400
-    file = request.files['file']
-    if 'key' not in request.form:
-        return "No se seleccionó ninguna clave", 400
-    key_name = request.form['key']
-    # Lee la clave privada
-    with open(os.path.join('claves', key_name), 'rb') as private_key_file:
-        private_key = f.import_private_key(private_key_file.read())
-    # Desencripta el archivo
-    decrypted_file = f.decrypt_file(private_key, file)
-    # Guarda el archivo desencriptado
-    with open(os.path.join('archivos', 'decrypted_' + file.filename), 'wb') as decrypted_file_file:
-        decrypted_file_file.write(decrypted_file)
-    return redirect(url_for('casimetrico'))
+    if request.method == 'POST':
+        operation = request.form.get('operation')
+        if operation == 'generate_keys':
+            private_key, public_key = f.generate_keys()
+            with open(os.path.join('claves', 'private_key.pem'), 'wb') as private_key_file:
+                private_key_file.write(f.export_private_key(private_key))
+            with open(os.path.join('claves', 'public_key.pem'), 'wb') as public_key_file:
+                public_key_file.write(f.export_public_key(public_key))
+            keys = os.listdir('claves')
+            return render_template("casimetrico.html", keys=keys)
+        elif operation == 'import_key':
+            public_key_file = request.files["public_key"]
+            public_key = f.import_public_key(public_key_file.read())
+            with open(os.path.join('claves', 'imported_public_key.pem'), 'wb') as imported_public_key_file:
+                imported_public_key_file.write(f.export_public_key(public_key))
+            return "Clave pública importada con éxito"
+        elif operation == 'export_key':
+            with open(os.path.join('claves', 'public_key.pem'), 'rb') as public_key_file:
+                public_key = f.import_public_key(public_key_file.read())
+            response = make_response(f.export_public_key(public_key))
+            response.headers.set('Content-Type', 'application/octet-stream')
+            response.headers.set('Content-Disposition', 'attachment', filename='public_key.pem')
+            return response
+        elif operation == 'encrypt_message':
+            message = request.form["message"].encode('utf-8')
+            with open(os.path.join('claves', 'public_key.pem'), 'rb') as public_key_file:
+                public_key = f.import_public_key(public_key_file.read())
+            encrypted_message = f.encrypt_message(public_key, message)
+            return jsonify({"encrypted_message": base64.b64encode(encrypted_message).decode('utf-8')})
+        elif operation == 'decrypt_message':
+            encrypted_message = base64.b64decode(request.form["encrypted_message"])
+            with open(os.path.join('claves', 'private_key.pem'), 'rb') as private_key_file:
+                private_key = f.import_private_key(private_key_file.read())
+            original_message = f.decrypt_message(private_key, encrypted_message)
+            return jsonify({"original_message": original_message.decode('utf-8')})
+        elif operation == 'encrypt_file':
+            if 'file' not in request.files:
+                return "No se subió ningún archivo", 400
+            file = request.files['file']
+            with open(os.path.join('claves', 'public_key.pem'), 'rb') as public_key_file:
+                public_key = f.import_public_key(public_key_file.read())
+            encrypted_file = f.encrypt_file(public_key, file)
+            with open(os.path.join('archivos', file.filename), 'wb') as encrypted_file_file:
+                encrypted_file_file.write(encrypted_file)
+            return redirect(url_for('casimetrico'))
+        elif operation == 'decrypt_file':
+            if 'file' not in request.files:
+                return "No se subió ningún archivo", 400
+            file = request.files['file']
+            if 'key' not in request.form:
+                return "No se seleccionó ninguna clave", 400
+            key_name = request.form['key']
+            with open(os.path.join('claves', key_name), 'rb') as private_key_file:
+                private_key = f.import_private_key(private_key_file.read())
+            decrypted_file = f.decrypt_file(private_key, file)
+            with open(os.path.join('archivos', 'decrypted_' + file.filename), 'wb') as decrypted_file_file:
+                decrypted_file_file.write(decrypted_file)
+            return redirect(url_for('casimetrico'))
+    else:
+        keys = os.listdir('claves')
+        files = os.listdir('archivos')
+        return render_template("casimetrico.html", keys=keys, files=files)
 
 @app.route("/about/")
 def about():
